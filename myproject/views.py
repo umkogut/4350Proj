88da60b619fac0864daf18bf9f13b6795bb02ceb
EC2 Default User <ec2-user@domU-12-31-39-06-B4-08.compute-1.internal>
2013-02-11 01:46:19 +0000

    Got the nav bar working, using jinja2 properly (whole group involved)

--- myproject/views.py
+++ myproject/views.py
@@ -1,6 +1,27 @@
 from pyramid.view import view_config
+from jinja2 import Environment, FileSystemLoader
 
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='menu', renderer='templates/menu.jinja2')
+def cook_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='orders', renderer='templates/orders.jinja2')
+def orders_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='admin', renderer='templates/admin.jinja2')
+def admin_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='about', renderer='templates/about.jinja2')
+def about_view(request):
+    print request
     return {'project': 'MyProject'}
