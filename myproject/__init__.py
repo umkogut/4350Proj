692bfe51463f8a2faf74d461d909c050e55e59bb
EC2 Default User <ec2-user@domU-12-31-39-06-B4-08.compute-1.internal>
2013-02-11 01:46:19 +0000

    Got the nav bar working, using jinja2 properly (whole group involved)

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -5,7 +5,13 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.add_jinja2_search_path("myproject:templates")
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('menu', '/menu')
+    config.add_route('orders', '/orders')
+    config.add_route('admin', '/admin')
+    config.add_route('about', '/about')
     config.scan()
     return config.make_wsgi_app()
