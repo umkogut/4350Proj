e0b4e2bcdfd82915a9b245246013b8197fc283cb
umchmie2 <umchmie2@cc.umanitoba.ca>
2013-02-20 09:06:32 -0600

    Changed database name and created menuItems table with one record in it.  Also modified the UI for the home page (index.jinja2)

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -2,11 +2,23 @@ from pyramid.config import Configurator
 from pyramid.events import subscriber
 from pyramid.events import NewRequest
 
+from sqlalchemy import engine_from_config
+
+from .models import (
+    DBSession,
+    Base,
+    )
+
 from urlparse import urlparse
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path("myproject:templates")
