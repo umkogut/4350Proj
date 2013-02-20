8a315d588259062677a9c85eefe4c1da8ea2fbdf
umchmie2 <umchmie2@cc.umanitoba.ca>
2013-02-20 09:06:32 -0600

    Changed database name and created menuItems table with one record in it.  Also modified the UI for the home page (index.jinja2)

--- myproject/views.py
+++ myproject/views.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
 from jinja2 import Environment, FileSystemLoader
 
+from .models import (
+    DBSession,
+    MenuItem,
+    )
+
 @view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
     print request
@@ -9,7 +14,13 @@ def my_view(request):
 @view_config(route_name='menu', renderer='templates/menu.jinja2')
 def cook_view(request):
     print request
-    return {'project': 'MyProject'}
+
+    print "Loading the menu"
+    item = DBSession.query(MenuItem).first()
+    if item is None:
+	print "There is nothing in the menu"
+
+    return {'item': item, 'project': 'MyProject'}
 
 @view_config(route_name='orders', renderer='templates/orders.jinja2')
 def orders_view(request):
