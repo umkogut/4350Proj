e2aa6881476124eaa78ae31e3061ba514baf2788
araujot@cc.umanitoba.ca <ec2-user@ip-10-243-119-104.ec2.internal>
2013-02-23 23:32:13 +0000

    The orders page now make reference to the database

--- myproject/views.py
+++ myproject/views.py
@@ -50,7 +50,10 @@ def placeOrder_view(request):
 @view_config(route_name='orders', renderer='templates/orders.jinja2')
 def orders_view(request):
     print request
-    return {'project': 'MyProject'}
+
+    menuItems = DBSession.query(MenuItem).group_by(MenuItem.category,MenuItem.name).all()
+
+    return {'menuItems': menuItems, 'project': 'MyProject'}
 
 @view_config(route_name='pos', renderer='templates/pos.jinja2')
 def pos_view(request):
