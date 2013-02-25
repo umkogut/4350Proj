5b317f935c10d3ae151228910416770b6c6984cb
umkalicm <umkalicm@cc.umanitoba.ca>
2013-02-24 21:06:22 -0600

    Halfway to setting up the database properly and adding menu items. Rong will be able to fix things in this branch to have it working properly

--- myproject/models.py
+++ myproject/models.py
@@ -4,7 +4,8 @@ from sqlalchemy import (
     Numeric,
     String,
     BOOLEAN,
-    ForeignKey
+    ForeignKey,
+	REAL
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -19,18 +20,92 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MenuItem(Base):
-    __tablename__ = 'menuItems'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    category = Column(String(50), nullable=False)
-    price = Column(Numeric, nullable=False)
-    isVegetarian = Column(BOOLEAN, nullable=False)
-    isActive = Column(BOOLEAN, nullable=False)
+class MenuCategory(Base):
+	__tablename__ = 'MENU_CATEGORIES'
+	catID = Column(Integer, primary_key=True)
+	name = Column(String, nullable=False)
 
-    def __init__(self, name, category, price, isVegetarian, isActive):
-        self.name = name
-	self.category = category
-	self.price = price
-	self.isVegetarian = isVegetarian
-	self.isActive = isActive
+	def __init__(self, name):
+		self.name = name
+
+class MenuItem(Base):
+	__tablename__ = 'MENU'
+	menuID = Column(Integer, primary_key=True)
+	name = Column(String, nullable=False)
+	category = Column(Integer, ForeignKey("MENU_CATEGORIES.catID"), nullable=False)
+	price = Column(REAL, nullable=False)
+	isVeg = Column(BOOLEAN, nullable=False)
+	isActive = Column(BOOLEAN, nullable=False)
+	description = Column(String, nullable=True)
+	image = Column(String, nullable=True)
+
+	def __init__(self, name, category, price, isVeg, isActive, description, image):
+		self.name = name
+		self.category = category
+		self.price = price
+		self.isVeg = isVeg
+		self.isActive = isActive
+		self.description = description
+		self.image = image
+
+'''
+class Order(Base):
+	__tablename__ = 'ORDERS'
+	orderID = Column(Integer, primary_key=True)
+	menuItem = Column(Integer, ForeignKey("MENU.menuID"), nullable=False)
+	tableNum = Column(Integer, nullable=False)
+	groupNum = Column(Integer, nullable=False)
+	isComplete = Column(BOOLEAN, nullable=False)
+	comments = Column(String, nullable=True)
+
+	def __init__(self, menuItem, tableNum, groupNum, comments):
+		self.menuItem = menuItem
+		self.tableNum = tableNum
+		self.groupNum = groupNum
+		self.isComplete = False
+		self.comments = comments
+'''
+
+class PayMethod(Base):
+	__tablename__ = 'PAY_METHODS'
+	methodID = Column(Integer, primary_key=True)
+	name = Column(String, nullable=False)
+
+	def __init__(self, name):
+		self.name = name
+
+class UserType(Base):
+	__tablename__ = 'USER_TYPES'
+	typeID = Column(Integer, primary_key=True)
+	name = Column(String, nullable=False)
+
+	def __init__(self, name):
+		self.name = name
+
+class User(Base):
+	__tablename__ = 'USERS'
+	userID = Column(Integer, primary_key=True)
+	userName = Column(String, nullable=False)
+	userType = Column(Integer, ForeignKey("USER_TYPES.typeID"), nullable=False)
+	password = Column(String, nullable=False)
+
+	def __init__(self, userName, userType, password):
+		self.userName = userName
+		self.userType = userType
+		self.password = password
+
+'''
+class Transaction(Base):
+	__tablename__ = 'TRANSACTIONS'
+	transactionID = Column(Integer, primary_key=True)
+	amount = Column(REAL, nullable=False)
+	paymentMethod = Column(Integer, ForeignKey("PAY_METHODS.methodID"), nullable=False)
+	whenProcessed = Column(Numeric, nullable=False)
+	userID = Column(Integer, ForeignKey("USERS.userID"), nullable=False)
+
+	def __init__(self, amount, paymentMethod, whenProcessed, userID):
+		self.amount = amount
+		self.paymentMethod = paymentMethod
+		self.whenProcessed = whenProcessed
+		self.userID = userID
+'''
