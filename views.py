95c2b4e2d725754763bb01bec161e682297ac9d6
Tiago Martins Araújo <araujot@cc.umanitoba.ca>
2013-02-28 13:24:27 -0600

    Revert "Removing trash files"
    This reverts commit e9707f1032e997b164dd79f59370ae4b026a7ac0.

---
+++ views.py
@@ -0,0 +1,93 @@
+import pdb
+import transaction;
+
+from pyramid.response import Response
+from pyramid.view import view_config
+from jinja2 import Environment, FileSystemLoader
+
+from .models import (
+    DBSession,
+    MenuItem,
+    )
+
+@view_config(route_name='add_menu_item', renderer='templates/admin.jinja2')
+def add_menu_item(request):
+    print request
+
+    itemName = request.params['itemName']
+    desc = request.params['desc']
+    price = request.params['price']
+
+    newItem = MenuItem(name=itemName, category='Entrees', price=price, isVegetarian=False, isActive=True) 
+    DBSession.add(newItem)
+
+    return {'project': 'MyProject'}
+
+@view_config(route_name='home', renderer='templates/index.jinja2')
+def my_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='menu', renderer='templates/menu.jinja2')
+def cook_view(request):
+    print request
+
+    print "Loading the menu"
+    menuItems = DBSession.query(MenuItem).group_by(MenuItem.category, MenuItem.name).all()
+    
+    print menuItems
+
+    if menuItems is None:
+	print "There is nothing in the menu"
+
+    return {'menuItems': menuItems, 'project': 'MyProject'}
+
+@view_config(route_name='placeOrder', renderer='templates/placeOrder.jinja2')
+def placeOrder_view(request):
+	print request
+	return {'project': 'MyProject'}
+
+@view_config(route_name='orders', renderer='templates/orders.jinja2')
+def orders_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='pos', renderer='templates/pos.jinja2')
+def pos_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='admin', renderer='templates/admin.jinja2')
+def admin_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='about', renderer='templates/about.jinja2')
+def about_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+@view_config(route_name='test', renderer='templates/test.jinja2')
+def test_view(request):
+    print request
+    return {'project': 'MyProject'}
+
+"""
+Keeping this code around temporarily. Will need to look at it later.
+Just keep pushing it to the bottom when adding new views
+- Marko
+
+@view_config(route_name='fktest', renderer='templates/fktest.jinja2')
+def fktest_view(request):
+    newTestFK = testFK(fkID=1, testID=1)
+    DBSession.add(newTestFK)
+
+    transaction.commit()
+
+    newTestFK2 = testFK(fkID=2, testID=2)
+    DBSession.add(newTestFK2)
+
+    transaction.commit()
+
+    return {'project': 'MyProject'}
+"""
