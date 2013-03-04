3797f5eab80807d9d47d2fc2beffa664471a0b59
araujot <araujot@cc.umanitoba.ca>
2013-03-04 16:29:29 +0000

    Make the main js function run when the page loads

--- myproject/static/js/orders.js
+++ myproject/static/js/orders.js
@@ -1,27 +1,4 @@
-/*$(function() {
-        $(".appetizer").change(function() {
-                if ($('.appetizer:checked').length == $('.appetizer').length) {
-                        $('.entree').attr("disabled", false);
-
-                        if ($('.entree:checked').length == $('.entree').length) {
-                                $('.dessert').attr("disabled", false);
-                        }
-                } else {
-                        $('.entree').attr("disabled", true);
-                        $('.dessert').attr("disabled", true);
-                }
-        });
-        $(".entree").change(function() {
-                if ($('.entree:checked').length == $('.entree').length) {
-                        $('.dessert').attr("disabled", false);
-                } else {
-                        $('.dessert').attr("disabled", true);
-                }
-        });
-});
-*/
-
-$('#test').click(function() {
+$(document).ready(function() {
 	$.post('getOrders.json', function(data) {
 		var orderList = $.parseJSON(data);
 		$('#tableNums').empty();
@@ -42,22 +19,44 @@ $('#test').click(function() {
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Entrees')
-						$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '" disable="false">' + order.menuName + '</label></dd>');
 				});
 			else {
 				if (orders.category == 'Entrees')
-					$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
+					$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '" disable="false">' + orders.menuName + '</label></dd>');
 			}
 			$('#'+tableNum).children().children().children().children().append('</dl></div> <!-- span4 --><div class="span4"><dl><dt>Dessert</dt>');
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Dessert')
-						$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '" disable="false">' + order.menuName + '</label></dd>');
 				});
 			else
-					$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
+					$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '" disable="false">' + orders.menuName + '</label></dd>');
 			$('#'+tableNum).children().children().children().eq(1).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="submit" class="btn">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
 		});
 	}, "json");
+		
+
+	/*$(".appetizer").change(function() {
+		if ($('.appetizer:checked').length == $('.appetizer').length) {
+                        $('.entree').attr("disabled", false);
+			if ($('.entree:checked').length == $('.entree').length)
+				 $('.dessert').attr("disabled", false);
+                } else {
+                        $('.entree').attr("disabled", true);
+                        $('.dessert').attr("disabled", true);
+                }
+	 });
+        $(".entree").change(function() {
+                if ($('.entree:checked').length == $('.entree').length) {
+                        $('.dessert').attr("disabled", false);
+               	 } else {
+                        $('.dessert').attr("disabled", true);
+                }
+       	 });
+	*/
+	
+	
 });
 
