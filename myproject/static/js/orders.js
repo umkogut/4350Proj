20478e0e241f46e903e6ff6cad764cd2cb3b7995
araujot <araujot@cc.umanitoba.ca>
2013-03-04 15:24:27 +0000

    Fixed redundand itens name bug

--- myproject/static/js/orders.js
+++ myproject/static/js/orders.js
@@ -42,23 +42,21 @@ $('#test').click(function() {
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Entrees')
-						$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
 				});
 			else {
 				if (orders.category == 'Entrees')
-					$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
+					$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
 			}
 			$('#'+tableNum).children().children().children().children().append('</dl></div> <!-- span4 --><div class="span4"><dl><dt>Dessert</dt>');
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Dessert')
-						$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
 				});
-			else {
-				if (orders.category == 'Dessert')
-					$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
-			}
-			$('#'+tableNum).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="submit" class="btn">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
+			else
+					$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
+			$('#'+tableNum).children().children().children().eq(1).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="submit" class="btn">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
 		});
 	}, "json");
 });
