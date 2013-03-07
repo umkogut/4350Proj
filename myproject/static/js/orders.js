eb05d8701c97351bfb730f303b321a81bf1f293d
araujot <araujot@cc.umanitoba.ca>
2013-03-07 20:45:14 +0000

    Fixed some bugs

--- myproject/static/js/orders.js
+++ myproject/static/js/orders.js
@@ -9,54 +9,54 @@ $(document).ready(function() {
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Appetizers')
-						$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="appetizer" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(0).append('<dd><label class="checkbox"><input type="checkbox" class="appetizer" id="' + order.menuName + '">' + order.menuName + '</label></dd>');
 				});
 			else {
 				if (orders.category == 'Appetizers')
-					$('#'+tableNum).children().children().children().children().children().children().append('<dd><label class="checkbox"><input type="checkbox" class="appetizer" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
+					$('#'+tableNum).children().children().children().children().children().children().eq(0).append('<dd><label class="checkbox"><input type="checkbox" class="appetizer" id="' + orders.menuName + '">' + orders.menuName + '</label></dd>');
 			}
 			$('#'+tableNum).children().children().children().children().append('</dl></div> <!-- span4 --><div class="span4"><dl><dt>Entrees</dt>');
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Entrees')
-						$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '" disable="false">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + order.menuName + '" disabled="true">' + order.menuName + '</label></dd>');
 				});
 			else {
 				if (orders.category == 'Entrees')
-					$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '" disable="false">' + orders.menuName + '</label></dd>');
+					$('#'+tableNum).children().children().children().children().children().children().eq(1).append('<dd><label class="checkbox"><input type="checkbox" class="entree" id="' + orders.menuName + '" disabled="true">' + orders.menuName + '</label></dd>');
 			}
 			$('#'+tableNum).children().children().children().children().append('</dl></div> <!-- span4 --><div class="span4"><dl><dt>Dessert</dt>');
 			if (orders.constructor == Array)
 				$.each(orders, function(table, order) {
 					if (order.category == 'Dessert')
-						$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '" disable="false">' + order.menuName + '</label></dd>');
+						$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + order.menuName + '" disabled="true">' + order.menuName + '</label></dd>');
 				});
-			else
-					$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '" disable="false">' + orders.menuName + '</label></dd>');
+			else{
+				if (orders.category == 'Dessert')
+					$('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.menuName + '" disabled="true">' + orders.menuName + '</label></dd>');
+			}
 			$('#'+tableNum).children().children().children().eq(1).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="submit" class="btn">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
 		});
 	}, "json");
-		
+});
 
-	/*$(".appetizer").change(function() {
-		if ($('.appetizer:checked').length == $('.appetizer').length) {
+$(function() {
+	$(".appetizer").change(function() {
+                if ($('.appetizer:checked').length == $('.appetizer').length) {
                         $('.entree').attr("disabled", false);
-			if ($('.entree:checked').length == $('.entree').length)
-				 $('.dessert').attr("disabled", false);
-                } else {
+                        if ($('.entree:checked').length == $('.entree').length)
+                                 $('.dessert').attr("disabled", false);
+                } else {                                                                                                           
                         $('.entree').attr("disabled", true);
                         $('.dessert').attr("disabled", true);
-                }
-	 });
+                }                                                                                                                  
+         });
         $(".entree").change(function() {
                 if ($('.entree:checked').length == $('.entree').length) {
                         $('.dessert').attr("disabled", false);
-               	 } else {
+                 } else {
                         $('.dessert').attr("disabled", true);
                 }
-       	 });
-	*/
-	
-	
+         });
 });
 
