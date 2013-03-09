31f72aebdaa80acddfdb9dc69b1bc00e65b745c5
umkogut <umkogut@cc.umanitoba.ca>
2013-03-09 17:26:10 -0600

    Added JSON functionality to pass data to DB

--- myproject/static/js/placeOrder.js
+++ myproject/static/js/placeOrder.js
@@ -12,3 +12,24 @@ $(function() {
   }
 });
 
+function submitOrder(table) {
+  var selectMenu = $('#orderlist' + table).val();
+  var result = '{ "Orders": [';
+  for( var i = 0; i < selectMenu.length; i++)
+  {
+    if (i != 0)
+    {
+      result = result + ',';
+    }
+    result= result + '{ "menuItem":' + selectMenu[i] + ', "tableNum":' + table;
+    result= result + ', "groupNum":0, "comments":""}';
+  }
+  result= result + ']}';
+
+  var jsonResult = $.parseJSON(result);
+  jsonResult = JSON.stringify(jsonResult);
+  $.post('/placedOrder.json', jsonResult, function(data) {
+  }, "json");
+  alert("Order placed");
+}
+
