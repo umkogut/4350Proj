5293bb0a80f63ecc39a7f6b6dac428f2d76a2def
umwur <umwur@cc.umanitoba.ca>
2013-03-11 17:56:52 +0000

    view orders now updates database with status

--- myproject/static/js/orders.js
+++ myproject/static/js/orders.js
@@ -1,3 +1,5 @@
+var table;
+
 $(document).ready(function() {
         $.post('getOrders.json', function(data) {
                 var orderList = $.parseJSON(data);
@@ -59,28 +61,47 @@ $(document).ready(function() {
                                                 $('#'+tableNum).children().children().children().children().children().children().eq(2).append('<dd><label class="checkbox"><input type="checkbox" class="dessert" id="' + orders.orderID + '" checked>' + orders.menuName + '</label></dd>');
                                 }
                         }
-                        $('#'+tableNum).children().children().children().eq(1).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="submit" class="btn" id="button">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
+                        $('#'+tableNum).children().children().children().eq(1).append('</dl></div> <!-- span4 --></div> <!-- row-fluid --><button type="button" class="btn" id="submitBtn" name="submitBtn">Submit</button></div> <!-- container-fluid --></fieldset></form></div>');
                 });
         }, "json");
-});                                                                                                                                
-/*                                                                                                                                 
-var checkedItens;                                                                                                                  
-                                                                                                                                   
-$(document).on('input:checked', '.checkbox', function(){                                                                           
-        checkedItens.push(JSON.stringify({
-                        'name': $(this).val(),
-                        'category': $(this).class(),
-                        'group': $(this).id()
-                        });
-        );                                                                                                                         
-});                                                                                                                                
-                                                                                                                                   
-                                                                                                                                   
-$(document).bind('click', '.checkbox', fuction(e){
-        alert(e.target.id);                                                                                                        
-});                                                                                                                                
-*/                                                                                                                                 
-$(document).on('click', '#button', function(){                                                                                     
-        alert("I work!");                                                                                                          
 });
-                                                                                                                                   
+
+$(document).on('shown', '#tableNums', function(e) {
+	table = e.target.text;
+	$('#result').text("");
+	updateCheckbox();
+});
+
+$(document).on('change', 'input:checkbox', function() {
+	updateCheckbox();
+});
+
+$(document).on('click', ':button[name="submitBtn"]', function() {
+	var status = '{"orderStatus": [' + getOrderStatus() + ']}';
+	alert(status);
+	$.post('/orderStatus.json', status, function(data) {
+		if(data['isSuccess'])
+			$('#result').text('Successfully updated order for "' + table + '"');
+	}, "json");
+});
+
+function getOrderStatus() {
+	var status = $('#' + table + ' input:checkbox').map( function() {
+		return '{"orderID": ' + this.id + ', "isComplete": ' + this.checked + '}';
+	}).get().join(',');
+	return status;
+}
+
+function updateCheckbox() {
+	if ($('#' + table + ' input:checkbox.appetizer:not(:checked)').length == 0) {
+		$('#' + table + ' input:checkbox.entree').attr("disabled", false);
+		if ($('#' + table + ' input:checkbox.entree:not(:checked)').length == 0)
+			$('#' + table + ' input:checkbox.dessert').attr("disabled", false);
+		else
+			$('#' + table + ' input:checkbox.dessert').attr("disabled", true);
+	}
+	else {
+		$('#' + table + ' input:checkbox.entree').attr("disabled", true);
+		$('#' + table + ' input:checkbox.dessert').attr("disabled", true);
+	}
+}
