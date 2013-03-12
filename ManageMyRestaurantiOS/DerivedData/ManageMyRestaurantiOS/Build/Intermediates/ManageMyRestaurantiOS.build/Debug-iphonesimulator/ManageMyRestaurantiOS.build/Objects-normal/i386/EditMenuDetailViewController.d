fd4258cf798cd72f601bb69fde2ace96908378e4
umdrawa2 <umdrawa2@cc.umanitoba.ca>
2013-03-12 14:53:13 -0500

    Add in UICustomSwitchFolder

--- ManageMyRestaurantiOS/DerivedData/ManageMyRestaurantiOS/Build/Intermediates/ManageMyRestaurantiOS.build/Debug-iphonesimulator/ManageMyRestaurantiOS.build/Objects-normal/i386/EditMenuDetailViewController.d
+++ ManageMyRestaurantiOS/DerivedData/ManageMyRestaurantiOS/Build/Intermediates/ManageMyRestaurantiOS.build/Debug-iphonesimulator/ManageMyRestaurantiOS.build/Objects-normal/i386/EditMenuDetailViewController.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/EditMenuDetailViewController.m \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/EditMenuDetailViewController.h
+  /home/student/umdrawa2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/EditMenuDetailViewController.m \
+  /home/student/umdrawa2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/EditMenuDetailViewController.h
---
+++ ManageMyRestaurantiOS/DerivedData/ManageMyRestaurantiOS/Build/Intermediates/ManageMyRestaurantiOS.build/Debug-iphonesimulator/ManageMyRestaurantiOS.build/Objects-normal/i386/UICustomSwitch.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /home/student/umdrawa2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/../../../Downloads/UICustomSwitch/UICustomSwitch/UICustomSwitch.m \
+  /home/student/umdrawa2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/../../../Downloads/UICustomSwitch/UICustomSwitch/UICustomSwitch.h
