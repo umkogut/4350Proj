cc86c9399cb828d4593a0e1fd5b8c98d1334dda1
umkalicm <umkalicm@cc.umanitoba.ca>
2013-03-17 16:24:26 -0500

    Initial start on iOS testing

---
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/ManageMyRestaurantiOSTests.m
@@ -0,0 +1,32 @@
+//
+//  ManageMyRestaurantiOSTests.m
+//  ManageMyRestaurantiOSTests
+//
+//  Created by Marko Kalic on 3/17/13.
+//  Copyright (c) 2013 Group6Comp4350. All rights reserved.
+//
+
+#import "ManageMyRestaurantiOSTests.h"
+
+@implementation ManageMyRestaurantiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ManageMyRestaurantiOSTests");
+}
+
+@end
