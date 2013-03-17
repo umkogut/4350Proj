b4b647e803a47c081ccf71cc90342cb44d769f1b
umchmie2 <umchmie2@cc.umanitoba.ca>
2013-03-16 22:09:15 -0500

    Fixed cancel navigation for the Add and Edit view controllers.

--- ManageMyRestaurantiOS/ManageMyRestaurantiOS/MenuDetailViewController.h
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOS/MenuDetailViewController.h
@@ -11,8 +11,6 @@
 @class MenuItem;
 
 @interface MenuDetailViewController : UITableViewController
-- (IBAction)EditMenuItemDetails:(id)sender;
-
 @property (strong, nonatomic) MenuItem *menuItem;
 
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
@@ -22,5 +20,6 @@
 @property (weak, nonatomic) IBOutlet UITableViewCell *isVegCell;
 
 - (IBAction)done:(UIStoryboardSegue *)segue;
+- (IBAction)cancel:(UIStoryboardSegue *)segue;
 
 @end
