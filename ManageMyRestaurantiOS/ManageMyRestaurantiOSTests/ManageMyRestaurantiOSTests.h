138fa2f15ee3c6c87f7f1542f320a773c22fe0a7
umkalicm <umkalicm@cc.umanitoba.ca>
2013-03-17 16:24:26 -0500

    Initial start on iOS testing

---
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/ManageMyRestaurantiOSTests.h
@@ -0,0 +1,13 @@
+//
+//  ManageMyRestaurantiOSTests.h
+//  ManageMyRestaurantiOSTests
+//
+//  Created by Marko Kalic on 3/17/13.
+//  Copyright (c) 2013 Group6Comp4350. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ManageMyRestaurantiOSTests : SenTestCase
+
+@end
