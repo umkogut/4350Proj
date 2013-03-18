61439e93c2624e931775e5dd0e540f4f29610246
umwur <umwur@cc.umanitoba.ca>
2013-03-18 15:44:04 -0500

    cleaning up repo

--- ManageMyRestaurantiOS/DerivedData/ManageMyRestaurantiOS/Build/Intermediates/ManageMyRestaurantiOS.build/Debug-iphoneos/ManageMyRestaurantiOS.build/Objects-normal/armv7/AddMenuItemViewController.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/AddMenuItemViewController.m \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/AddMenuItemViewController.h \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/UICustomSwitch/UICustomSwitch/UICustomSwitch.h
--- ManageMyRestaurantiOS/DerivedData/ManageMyRestaurantiOS/Build/Intermediates/ManageMyRestaurantiOS.build/Debug-iphoneos/ManageMyRestaurantiOS.build/Objects-normal/armv7/MenuItemDataController.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/MenuItemDataController.m \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/MenuItemDataController.h \
-  /home/student/umchmie2/4350Proj/ManageMyRestaurantiOS/ManageMyRestaurantiOS/MenuItem.h
