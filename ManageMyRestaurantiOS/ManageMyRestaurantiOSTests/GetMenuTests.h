e2d681b7809930fd6c83d84617128bcde5c23b54
umkalicm <umkalicm@cc.umanitoba.ca>
2013-03-17 20:55:44 -0500

    iOS testing on creating a new menu item and adding to the menu list

---
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/GetMenuTests.h
@@ -0,0 +1,13 @@
+//
+//  GetMenuTests.h
+//  ManageMyRestaurantiOS
+//
+//  Created by Marko Kalic on 3/17/13.
+//  Copyright (c) 2013 Group6Comp4350. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GetMenuTests : SenTestCase
+
+@end
---
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/GetMenuTests.m
@@ -0,0 +1,13 @@
+//
+//  GetMenuTests.m
+//  ManageMyRestaurantiOS
+//
+//  Created by Marko Kalic on 3/17/13.
+//  Copyright (c) 2013 Group6Comp4350. All rights reserved.
+//
+
+#import "GetMenuTests.h"
+
+@implementation GetMenuTests
+
+@end
