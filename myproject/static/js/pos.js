8b2abfa753459b329db954cd19b2ef99b0159386
umdrawa2 <umdrawa2@cc.umanitoba.ca>
2013-03-22 01:04:13 +0000

    JS and template modifications for pos

--- myproject/static/js/pos.js
+++ myproject/static/js/pos.js
@@ -3,17 +3,41 @@ $(function() {
     $('#'+this.id+'_lb').toggle($(this).is(':checked'));
   });
  
-  $("#pay").click(function() {
+ /* $("#pay").click(function() {
     $("#bill input[type=checkbox]").each(function() {
       if($(this).is(":checked"))
       {
        $(this).parent().hide();
        $(this).prop('checked', false);
        $('#'+ this.id +'_lb').hide();
+     /* 
+       var itemsToPayFor = JSON.stringify({
+	'table':{% tableNum %},
+	'menuItem':'Food'});
+
+       $.post('/payForItems.json', itemsToPayFor, function(data) {
+	$('#posForm label[name="testing"]').text("Added stuff");	
+       }, "json");
       }
     });
-  });
+  });*/
 });
+
+function payForItems(tableNum) {
+	$("#bill input[type=checkbox]").each(function() {
+ 		if($(this).is(":checked")){
+			$(this).parent().hide();
+       			$(this).prop('checked', false);
+       			$('#'+ this.id +'_lb').hide();
+
+			var itemToPayFor = JSON.stringify({
+        			'table':tableNum,
+        			'menuItem':this.id});
+
+       			$.post('/payForItems.json', itemToPayFor, function(data) {}, "json");
+		}
+	});	
+}
 
 function displayTableBill(tableNum) {
     $(".tab-pane").hide();
