504ac8539d474770bfb533d3d88b2fa1429ee53a
araujot <araujot@cc.umanitoba.ca>
2013-03-22 22:45:25 +0000

    Fixed indentation error in views.py file

--- myproject/views.py
+++ myproject/views.py
@@ -142,13 +142,14 @@ def editMenuItem_view(request):
 		item.category = newItem['category']
 		item.price = newItem['price']
 		item.description = newItem['description']
-		item.image = newItem['image']
-        isVegetarian = item['isVeg']
-        if isVegetarian == 'TRUE':
-            item.isVeg = True
-        elif isVegetarian == 'FALSE':
-            item.isVeg = False
-            
+		item.image = newItem['image']            
+
+        	isVegetarian = item['isVeg']
+        	if isVegetarian == 'TRUE':
+			item['isVeg'] = True
+	        elif isVegetarian == 'FALSE':
+			item['isVeg'] = False
+
 		transaction.commit()
 		return {'isSuccess': 1}
 	else:
