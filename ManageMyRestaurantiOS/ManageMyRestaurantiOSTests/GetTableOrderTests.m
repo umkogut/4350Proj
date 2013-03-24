e408eb2d6635b6f897b8755e0610f12cee7bb170
Tiago Araujo <araujot@porcupine.cs.umanitoba.ca>
2013-03-24 16:10:47 -0500

    Tests for Table Order and files arrange fix

--- ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/GetTableOrderTests.m
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOSTests/GetTableOrderTests.m
@@ -16,6 +16,8 @@
 {
     [super setUp];
     
+    self.testTableOrder = [[TableOrder alloc] initWithTableNum:1];
+    
     NSString *name = @"testName";
     NSInteger orderID = 100;
     NSString *category = @"Apperitizer";
@@ -23,20 +25,23 @@
     BOOL isComplete = NO;
     NSString *comments = @"testComment";
     
-    //NSInteger tableNum = 1;
-    //NSMutableArray *orderList = [[NSMutableArray alloc] init];
-    
-    ItemOrder *order = [[ItemOrder alloc] initWithName:name orderID:orderID category:category groupNum:groupNum isComplete:isComplete comments:comments];
-    
+    self.order = [[ItemOrder alloc] initWithName:name orderID:orderID category:category groupNum:groupNum isComplete:isComplete comments:comments];
     
     self.nOrdersBefore = [self.testTableOrder numOrders];
-    NSLog(@" =BEFORE= %i", self.nOrdersBefore);
     
-    [self.testTableOrder addOrder:order];
+    [self.testTableOrder addOrder:self.order];
     
     self.nOrdersAfter = [self.testTableOrder numOrders];
     
-    NSLog(@" ==AFTER== %i", self.nOrdersAfter);
+    self.result = [[NSMutableArray alloc] init];
+    
+    self.result =[self.testTableOrder getListInCategory:@"Apperitizer"];
+    
+    self.resultOrder = [self.testTableOrder objectInListAtIndex:0];
+    
+    [self.testTableOrder clearOrders];
+    
+    self.nOrdersCleaned = [self.testTableOrder numOrders];
     
 }
 
@@ -45,26 +50,14 @@
     [super tearDown];
 }
 
-- (void)testNumOrder
+- (void)testOrderTests
 {
-    //STAssertTrue(self.nOrdersBefore == 0, @"Number of orders wrong before the insertion");
-    //STAssertTrue(self.nOrdersAfter == 1, @"Number of orders wrong after the insertion");
-    
-}
-
-- (void)testAddOrder
-{
-    
-}
-
-- (void)testGetListsInCategory
-{
-    
-}
-
-- (void)testObjectList
-{
-
+    STAssertNotNil(self.testTableOrder, @"Table was not allocated");
+    STAssertTrue(self.nOrdersBefore == 0, @"Number of orders wrong before the insertion");
+    STAssertTrue(self.nOrdersAfter == 1, @"Number of orders wrong after the insertion");
+    STAssertEquals(self.order, [self.result objectAtIndex:0], @"Orders is not returning right by category");
+    STAssertEquals(self.order, self.resultOrder, @"The order returned is different than the first one");
+    STAssertTrue(self.nOrdersCleaned == 0, @"Number of orders wrong after cleaning");
 }
 
 @end
