81b179a93ddeca8bc300d806a9b520c2136b2f86
David Kogut <umkogut@chipmunk.cs.umanitoba.ca>
2013-03-25 12:47:40 -0500

    added some placeOrder iOS files [with Amandusia]

---
+++ ManageMyRestaurantiOS/ManageMyRestaurantiOS/PlaceOrderMasterViewController.h
@@ -0,0 +1,24 @@
+//
+//  PlaceOrderMasterViewController.h
+//  ManageMyRestaurantiOS
+//
+//  Created by David Kogut on 2013-03-25.
+//  Copyright (c) 2013 Group6Comp4350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ASIHTTPRequest.h"
+#import "SBJson.h"
+
+@class ItemOrder;
+@class TableOrderDataController;
+@class PlaceOrderViewController;
+
+
+@interface PlaceOrderMasterViewController : UITableViewController
+
+@property (retain, nonatomic) TableOrderDataController *dataController;
+@property (strong, nonatomic) PlaceOrderViewController *detailViewController;
+
+@end
+
